b0ae1f6c49fe5eef4704bf9caeab67443f3be829
zkxprr13 <nghk4njkhx@privaterelay.appleid.com>
2026-02-25 17:44:36 +0300

    Add THREE.js and GLTFLoader imports

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+
 console.log("main.js started");
 document.body.insertAdjacentHTML(
   "beforeend",
